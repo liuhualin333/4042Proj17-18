d658beb00f225d2ccd35eeaed241031165cdc53f
HuaBa <liuhualin333@gmail.com>
2017-09-25 15:43:44 +0800

    Add feature scaling and finish splitint dataset

--- 4042Prj2.py
+++ 4042Prj2.py
@@ -1,14 +1,38 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import math
 
-filename_queue = tf.train.string_input_producer(["cal_housing.data", "cal_housing.domain"])
+#Read csv file and convert it to tf tensor with random shuffle done
+def read_files(filename):
+	featUseCols = (0,1,2,3,4,5,6,7)
+	labelUseCols = (8)
+	colNames = "feat0,feat1,feat2,feat3,feat4,feat5,feat6,feat7,label"
+	features = np.genfromtxt(filename, delimiter=',',autostrip=True, usecols=featUseCols)
+	labels = np.genfromtxt(filename, delimiter=',',autostrip=True, usecols=labelUseCols)
 
-reader = tf.TextLineReader()
-feature, label = reader.read(filename_queue)
+	#Random permutation of data
+	idx = np.random.permutation(len(features))
 
-features = tf.stack([tf.decode_csv(feature)])
+	#Perform feature scaling with zero mean and gaussian distribution
+	means, variances = tf.nn.moments(tf.stack(features[idx]), [0])
+	normalizedFeat = (tf.stack(features[idx]) - means) / tf.sqrt(variances)
+	
+	return normalizedFeat,tf.stack(labels[idx]),means,variances
+
+#Construct trainSet and testSet according to 7:3 ratio
+def split_train_test(features,labels,trainSetNum,testSetNum):
+	trainLabels,testLables = tf.split(labels, [trainSetNum,testSetNum])
+	trainData,testData = tf.split(features, [trainSetNum,testSetNum])
+	return trainData,testData,trainLabels,testLables
+
+features,labels,means,variances = read_files("cal_housing.data")
+
+with tf.Session() as sess:
+	trainSetNum = int(math.ceil(0.7*sess.run(tf.shape(labels))[0]))
+	testSetNum = sess.run(tf.shape(labels))[0] - trainSetNum
+	trainData,testData,trainLabels,testLabels = split_train_test(features,labels,trainSetNum,testSetNum)
+
+
 
 
 
