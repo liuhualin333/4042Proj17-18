7f7ae11ed5f435a70c6227df561031de4ea76101
Liu Hualin <liuhualin333@gmail.com>
2017-09-30 11:15:28 +0800

    add accuracy and test accuracy

--- 4042Prj2.py
+++ 4042Prj2.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import math
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 #Read csv file and convert it to tf tensor with random shuffle done
 def read_files(filename):
 	featUseCols = (0,1,2,3,4,5,6,7)
@@ -26,7 +27,6 @@ def split_train_test(features,labels,trainSetNum):
 	training_idx, test_idx = indices[:trainSetNum], indices[trainSetNum:]
 	trainLabels,testLables = labels[training_idx], labels[test_idx]
 	trainData,testData = features[training_idx,:], features[test_idx,:]
-	print(trainData,trainLabels)
 	return trainData,testData,trainLabels,testLables
 
 #Further split to k fold
@@ -41,32 +41,43 @@ def question1(train_feat,train_labels,test_feat,test_labels,dataSize):
 	labels = np.concatenate((train_labels,test_labels),axis=0)
 	output_max = np.amax(labels)
 	output_min = np.amin(labels)
+	print (output_max,output_min)
 	
 	with tf.name_scope('input'):
 		x = tf.placeholder('float64', shape=[None,8])
 		y = tf.placeholder('float64', shape=[None,1])
 	with tf.name_scope('weights'):
 		v = tf.Variable(np.random.randn(8,30)*.01,dtype='float64',name='h_weight')
+		#v1 = tf.Variable(tf.truncated_normal([30,20],dtype='float64'),name='h_weight')
+		#v2 = tf.Variable(tf.truncated_normal([20,20],dtype='float64'),name='h_weight')
 		w = tf.Variable(np.random.randn(30,1)*.01,dtype='float64',name='o_weight')
 	with tf.name_scope('biases'):	
 		bh = tf.Variable(tf.zeros([1],dtype='float64'),name='h_bias')
+		#bh1 = tf.Variable(tf.zeros([1],dtype='float64'),name='h_bias')
+		#bh2 = tf.Variable(tf.zeros([1],dtype='float64'),name='h_bias')
 		bo = tf.Variable(tf.zeros([1],dtype='float64'),name='o_bias')
-	with tf.name_scope('h_synaptic'):
 		syn_h = tf.matmul(x,v)+bh
-	with tf.name_scope('h_activation'):
 		act_h = tf.sigmoid(syn_h)
+		#syn_h1 = tf.matmul(act_h,v1)+bh1
+		#act_h1 = tf.sigmoid(syn_h1)
+		#syn_h2 = tf.matmul(act_h1,v2)+bh2
+		#act_h2 = tf.sigmoid(syn_h2)
 	with tf.name_scope('o_synaptic'):
 		syn_o = tf.matmul(act_h,w)+bo
 	with tf.name_scope('o_activation'):
-		act_o = (output_max-output_min)*tf.sigmoid(syn_o) + output_min
+		#act_o = (output_max-output_min)*tf.sigmoid(syn_o) + output_min
+		act_o = syn_o
 	with tf.name_scope('delta'):
 		delta = tf.reduce_mean(tf.square(y-act_o))
+		accu = tf.reduce_mean(act_o-y)
 	init = tf.global_variables_initializer()
 
 
-	learningRate = 0.0001
+	learningRate = 0.001
 	epoch = 1000
 	learningerror = np.zeros(epoch)
+	trainaccuracy = np.zeros(epoch)
+	testaccuracy = np.zeros(epoch)
 
 	with tf.name_scope('train'):
 		trainStep = tf.train.GradientDescentOptimizer(learningRate).minimize(delta)
@@ -78,14 +89,26 @@ def question1(train_feat,train_labels,test_feat,test_labels,dataSize):
 			batches = np.array_split(data,list(np.arange(32,dataSize,32)))
 			for batch in batches:
 				_,batch_delta = sess.run([trainStep,delta], {x:batch[:,:8], y:batch[:,8:]})
-				learningerror[i]+=batch_delta/train_feat.shape[0]
-			print("Dataset finished[%d]: %lf" % (i,learningerror[i]))
+			_,train_delta,train_accu = sess.run([trainStep,delta,accu], {x:trainData[:,:8], y:trainLabels[:].reshape(-1,1)})
+			_,test_delta,test_accu = sess.run([trainStep,delta,accu], {x:testData[:,:8], y:testLabels[:].reshape(-1,1)})
+			learningerror[i] = train_delta
+			trainaccuracy[i] = train_accu
+			testaccuracy[i] = test_accu
+			print("Dataset finished[%d]: %lf %lf %lf" % (i,learningerror[i], trainaccuracy[i], testaccuracy[i]))
 
 	plt.figure()
 	plt.plot(np.arange(epoch),learningerror)
 	plt.title('Training Error')
 	plt.savefig('figure_prj1.2.q1.png')
 	plt.show();
+
+	plt.figure()
+	plt.plot(np.arange(epoch), testaccuracy)
+	plt.xlabel('Epochs')
+	plt.ylabel('Accuracy')
+	plt.title('Test Accuracy')
+	plt.savefig('p_1b_sample_accuracy.png')
+	plt.show()
 
 features,labels,means,std = read_files("cal_housing.data")
 trainSetNum = int(math.ceil(0.7*labels.shape[0]))
