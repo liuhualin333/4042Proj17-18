5e0a05e38139e8102eecb2c2a2d50c0b89273449
Liu Hualin <liuhualin333@gmail.com>
2017-09-30 12:52:03 +0800

    doing nothing

--- 4042Prj2.py
+++ 4042Prj2.py
@@ -3,6 +3,13 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
+def scale(X, X_min, X_max):
+    return (X - X_min)/(X_max - X_min)
+ 
+def normalize(X, X_mean, X_std):
+    return (X - X_mean)/X_std
+
 #Read csv file and convert it to tf tensor with random shuffle done
 def read_files(filename):
 	featUseCols = (0,1,2,3,4,5,6,7)
@@ -10,24 +17,28 @@ def read_files(filename):
 	colNames = "feat0,feat1,feat2,feat3,feat4,feat5,feat6,feat7,label"
 	features = np.genfromtxt(filename, delimiter=',',autostrip=True, usecols=featUseCols)
 	labels = np.genfromtxt(filename, delimiter=',',autostrip=True, usecols=labelUseCols)
-
-	#Perform data normalization and feature scaling
-	means = np.mean(features,0)
-	std = np.mean(features)
-	normalizedFeat = data_normalization(features,means,std)
-	return normalizedFeat,labels,means,std
-
-def data_normalization(features,means,std):
-	normalizedFeat = (features - means) / std
-	return normalizedFeat
+	return features,labels
 
 #Construct trainSet and testSet according to 7:3 ratio
 def split_train_test(features,labels,trainSetNum):
 	indices = np.random.permutation(features.shape[0])
 	training_idx, test_idx = indices[:trainSetNum], indices[trainSetNum:]
 	trainLabels,testLables = labels[training_idx], labels[test_idx]
-	trainData,testData = features[training_idx,:], features[test_idx,:]
-	return trainData,testData,trainLabels,testLables
+	trainX,testX = features[training_idx,:], features[test_idx,:]
+	# scale and normalize data
+	trainX_max, trainX_min =  np.max(trainX, axis=0), np.min(trainX, axis=0)
+	testX_max, testX_min =  np.max(testX, axis=0), np.min(testX, axis=0)
+
+	trainX = scale(trainX, trainX_min, trainX_max)
+	testX = scale(testX, testX_min, testX_max)
+
+	trainX_mean, trainX_std = np.mean(trainX, axis=0), np.std(trainX, axis=0,keepdims=True)
+	testX_mean, testX_std = np.mean(testX, axis=0), np.std(testX, axis=0,keepdims=True)
+
+	trainX = normalize(trainX, trainX_mean, trainX_std)
+	testX = normalize(testX, testX_mean, testX_std)
+
+	return trainX,testX,trainLabels,testLables
 
 #Further split to k fold
 """def split_k_fold(features,labels,k):
@@ -39,37 +50,34 @@ def question1(train_feat,train_labels,test_feat,test_labels,dataSize):
 	data = np.concatenate((train_feat,train_labels.reshape(-1,1)),axis=1)
 	batches = np.array_split(data,list(np.arange(32,dataSize,32)))
 	labels = np.concatenate((train_labels,test_labels),axis=0)
-	output_max = np.amax(labels)
-	output_min = np.amin(labels)
-	print (output_max,output_min)
-	
-	with tf.name_scope('input'):
-		x = tf.placeholder('float64', shape=[None,8])
-		y = tf.placeholder('float64', shape=[None,1])
-	with tf.name_scope('weights'):
-		v = tf.Variable(np.random.randn(8,30)*.01,dtype='float64',name='h_weight')
-		#v1 = tf.Variable(tf.truncated_normal([30,20],dtype='float64'),name='h_weight')
-		#v2 = tf.Variable(tf.truncated_normal([20,20],dtype='float64'),name='h_weight')
-		w = tf.Variable(np.random.randn(30,1)*.01,dtype='float64',name='o_weight')
-	with tf.name_scope('biases'):	
-		bh = tf.Variable(tf.zeros([1],dtype='float64'),name='h_bias')
-		#bh1 = tf.Variable(tf.zeros([1],dtype='float64'),name='h_bias')
-		#bh2 = tf.Variable(tf.zeros([1],dtype='float64'),name='h_bias')
-		bo = tf.Variable(tf.zeros([1],dtype='float64'),name='o_bias')
-		syn_h = tf.matmul(x,v)+bh
-		act_h = tf.sigmoid(syn_h)
-		#syn_h1 = tf.matmul(act_h,v1)+bh1
-		#act_h1 = tf.sigmoid(syn_h1)
-		#syn_h2 = tf.matmul(act_h1,v2)+bh2
-		#act_h2 = tf.sigmoid(syn_h2)
-	with tf.name_scope('o_synaptic'):
-		syn_o = tf.matmul(act_h,w)+bo
-	with tf.name_scope('o_activation'):
-		#act_o = (output_max-output_min)*tf.sigmoid(syn_o) + output_min
-		act_o = syn_o
-	with tf.name_scope('delta'):
-		delta = tf.reduce_mean(tf.square(y-act_o))
-		accu = tf.reduce_mean(act_o-y)
+	with tf.device('/gpu:0'):
+		with tf.name_scope('input'):
+			x = tf.placeholder('float64', shape=[None,8])
+			y = tf.placeholder('float64', shape=[None,1])
+		with tf.name_scope('weights'):
+			v = tf.Variable(np.random.randn(8,30)*.01,dtype='float64',name='h_weight')
+			#v1 = tf.Variable(tf.truncated_normal([30,20],dtype='float64'),name='h_weight')
+			#v2 = tf.Variable(tf.truncated_normal([20,20],dtype='float64'),name='h_weight')
+			w = tf.Variable(np.random.randn(30,1)*.01,dtype='float64',name='o_weight')
+		with tf.name_scope('biases'):	
+			bh = tf.Variable(0.1,dtype='float64',name='h_bias')
+			#bh1 = tf.Variable(tf.zeros([1],dtype='float64'),name='h_bias')
+			#bh2 = tf.Variable(tf.zeros([1],dtype='float64'),name='h_bias')
+			bo = tf.Variable(0.1,dtype='float64',name='o_bias')
+			syn_h = tf.matmul(x,v)+bh
+			act_h = tf.sigmoid(syn_h)
+			#syn_h1 = tf.matmul(act_h,v1)+bh1
+			#act_h1 = tf.sigmoid(syn_h1)
+			#syn_h2 = tf.matmul(act_h1,v2)+bh2
+			#act_h2 = tf.sigmoid(syn_h2)
+		with tf.name_scope('o_synaptic'):
+			syn_o = tf.matmul(act_h,w)+bo
+		with tf.name_scope('o_activation'):
+			#act_o = (output_max-output_min)*tf.sigmoid(syn_o) + output_min
+			act_o = syn_o
+		with tf.name_scope('delta'):
+			delta = tf.reduce_mean(tf.square(act_o-y))
+			accu = tf.reduce_mean(act_o-y)
 	init = tf.global_variables_initializer()
 
 
@@ -110,7 +118,7 @@ def question1(train_feat,train_labels,test_feat,test_labels,dataSize):
 	plt.savefig('p_1b_sample_accuracy.png')
 	plt.show()
 
-features,labels,means,std = read_files("cal_housing.data")
+features,labels = read_files("cal_housing.data")
 trainSetNum = int(math.ceil(0.7*labels.shape[0]))
 testSetNum = labels.shape[0] - trainSetNum
 print(features.shape,labels.shape)
